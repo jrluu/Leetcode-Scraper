86cbb837a18458ae49bd9035c164e8c4fb0643ef
Jonathan Luu <jrluu@ucdavis.edu>
2016-12-16 21:50:45 -0800

    Created functions to create a directory, make a file, and clean up the title.
    I am currently working on gathering the CODE.
    This will take longer than expected because each line is formatted inside of LeetCode, so the normal method of getting the attribute value will not work

--- code_push.py
+++ code_push.py
@@ -7,17 +7,15 @@ Author: Jonathan Luu
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+import os
+import re
+import sys
 
 algorithms_page_driver = webdriver.Chrome("./chromedriver")
 problem_page_driver = webdriver.Chrome("./chromedriver")
 
 '''
-# https://leetcode.com/accounts/login/
-# Go to https://leetcode.com/problemset/algorithms/
-# Filter by solved problems
-# show "All rows"
-
-# Use another browser to click on links
+TODO
 # Gather code
 # Create .cpp file for it
 
@@ -25,6 +23,54 @@ problem_page_driver = webdriver.Chrome("./chromedriver")
 # git commit -m "Pushed LeetCode into Repo"
 # git push origin master
 '''
+
+def urlify(s):
+    '''
+    Removes special characters and replaces the white space with a dash
+
+    :param s: string
+    :return: string
+    '''
+
+    #Remove all non-word characters (everything except numbers and letters)
+    s = re.sub(r"[^\w\s]", '', s)
+
+    # Replace all runs of whitespace with a single dash
+    s = re.sub(r"\s+", '-', s)
+
+    return s
+
+
+def make_directory(path):
+    '''
+    :param path: String
+    :return: NULL
+    '''
+    try:
+        os.makedirs(path)
+    except OSError:
+        if not os.path.isdir(path):
+            raise
+
+
+def create_file(title, code):
+    '''
+    Creates the file with the name of title, and inserts the code inside of it
+
+    :param title: string
+    :param code:  string
+    :return:
+    '''
+
+    title = urlify(title)
+    extension = ".cpp"
+
+    title = "./leet_code_solutions/" + title + extension
+
+    file = open(title, 'w')
+    file.write(code)
+    file.close()
+
 
 
 def sign_into_leetcode():
@@ -55,21 +101,38 @@ def sign_into_leetcode():
 
 
 def go_to_algorithms():
+    """
+    This function opens the algorithms and scrapes your code off each problem.
+    It then stores it into a file.
+
+    TODO:
+    Copy Data from Site
+
+    :return:
+    """
+
+    make_directory("./leet_code_solutions")
 
     #Opens algorithms page
     algorithms_page_driver.get( "https://leetcode.com/problemset/algorithms/")
 
+    '''
     #Shows only problems that are solved
     algorithms_page_driver.find_element_by_xpath('// *[ @ id = "question-app"] / div '
                                                  '/ div[2] / form / div[1] / div / select / option[2]').click()
-
+    '''
     #Opens every problem and then copies their data into a file
     table = algorithms_page_driver.find_element_by_class_name('reactable-data')
     for row in table.find_elements_by_tag_name("tr"):
         href = row.find_element_by_tag_name("a").get_attribute("href")
         problem_page_driver.get(href)
-        time.sleep(3)
 
+        #code = problem_page_driver.find_element_by_xpath('//textarea[@class = "ace_text-input"]').text()
+        code = problem_page_driver.find_element_by_xpath("/html/body/div[1]/div[5]/div[2]/div/div/div/div/textarea").get_attribute("value")
+        title = row.find_element_by_tag_name("a").text
+        create_file(title, code)
+
+        #time.sleep(3)
 
 if __name__ == "__main__":
 #    sign_into_leetcode()
