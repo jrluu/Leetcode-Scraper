9bc9991a7f80eaf2d7ce449b509fce4085734f4e
Jonathan Luu <jrluu@ucdavis.edu>
2016-12-16 19:15:24 -0800

    Initial push for code_push.py.
    This code will log in to Leetcode, gather all of your code for solved problems, put them into a file, and then push them.

--- code_push.py
+++ code_push.py
@@ -5,11 +5,13 @@ Author: Jonathan Luu
 '''
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 
 algorithms_page_driver = webdriver.Chrome("./chromedriver")
 problem_page_driver = webdriver.Chrome("./chromedriver")
 
-
+'''
 # https://leetcode.com/accounts/login/
 # Go to https://leetcode.com/problemset/algorithms/
 # Filter by solved problems
@@ -22,12 +24,29 @@ problem_page_driver = webdriver.Chrome("./chromedriver")
 # git add .
 # git commit -m "Pushed LeetCode into Repo"
 # git push origin master
+'''
 
 def sign_into_leetcode():
+    algorithms_page_driver.get("https://leetcode.com/accounts/logout")
+
+    use_facebook = raw_input("Do you want to use facebook to login?")
     username = raw_input("Enter your username: ")
     password = raw_input("Enter your password: ")
-    #need to logout
-    algorithms_page_driver.get("https://leetcode.com/accounts/login/")
+
+    if (use_facebook.lower() == "y"):
+        algorithms_page_driver.get("https://leetcode.com/accounts/facebook/login/")
+        algorithms_page_driver.find_element_by_xpath('// *[ @ id = "email"]').send_keys(username)
+        algorithms_page_driver.find_element_by_xpath('//*[@id="pass"]').send_keys(password)
+        algorithms_page_driver.find_element_by_xpath('//*[@id="pass"]').send_keys(Keys.ENTER)
+    else:
+        algorithms_page_driver.get("https://leetcode.com/accounts/login/")
+        algorithms_page_driver.find_element_by_xpath('// *[ @ id = "id_login"]').send_keys(username)
+        algorithms_page_driver.find_element_by_xpath('// *[ @ id = "id_password"]').send_keys(password)
+        algorithms_page_driver.find_element_by_xpath('// *[ @ id = "id_password"]').send_keys(Keys.ENTER)
+
+
+    time.sleep(5)
+
 
 def go_to_algorithms():
     algorithms_page_driver.get( "https://leetcode.com/problemset/algorithms/")
